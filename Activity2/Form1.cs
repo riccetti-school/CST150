6eb2a3effee98052ffbb3e7ba3377bed513777b2
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-09-05 18:30:53 -0500

    activity 2

---
+++ Activity2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var inches = double.Parse(textBox1.Text);
+                double cm = 0.0;
+
+                cm = inches * 2.54;
+                textBox2.Text = cm.ToString();
+            }
+            catch
+            {
+                label3.Text = "Error";
+            }
+        }
+    }
+}
