8fb87f1f760d25407c53356bad29df642ed0568e
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-10-03 10:10:20 -0500

    Activity 11

---
+++ Activity11/Dice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity11
+{
+    public class Dice
+    {
+        private int _sides = 6;
+
+        public Dice(int sides)
+        {
+            if (sides < 4)
+                throw new Exception("Minimum 4");
+
+            if (sides > 20)
+                throw new Exception("Max 20");
+
+            _sides = sides;
+        }
+
+        public int rollDie()
+        {
+            return new Random((int)DateTime.Now.Ticks).Next(4, _sides) + 1;
+        }
+    }
+}
---
+++ Activity11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Dice(6).rollDie();
+            System.Threading.Thread.Sleep(1000);
+            var b = new Dice(6).rollDie();
+
+            if (a == 1 && b == 1)
+                Console.WriteLine("Snake eyes!!!!!");
+
+            Console.WriteLine($"A: {a} - B: {b}");
+
+            Console.ReadKey();
+        }
+    }
+}
