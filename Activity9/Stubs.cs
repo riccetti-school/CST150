93669834134766916c7776193e6933b7a286ff13
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-10-03 09:49:07 -0500

    activity 9

---
+++ Activity9/Stubs.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity9
+{
+    /// <summary>
+    /// Write a class that contains the following “stubs” for methods used in measurement conversion and include a screenshot of your program running with successful output. You do not have to implement the methods.
+    /// </summary>
+    public class Stubs
+    {
+        /// <summary>
+        /// Write a method that takes two integers and displays their sum
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int SumTwoInts(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that takes five doubles and returns their average
+        /// </summary>
+        /// <returns></returns>
+        public double FiveDoubleAverage(double a, double b, double c, double d, double e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that returns the sum of two randomly generated integers
+        /// </summary>
+        /// <returns></returns>
+        public int ReturnSumOfTwoRandomInts()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Write a method that takes three integers and returns true if their sum is divisible by 3, false otherwise
+        /// </summary>
+        /// <returns></returns>
+        public bool ThreeIntsDivisableByThree(int a, int b, int c)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that takes two strings and displays the string that has fewer character
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string FewerCharacters(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that takes an array of doubles and returns the largest value in the array
+        /// </summary>
+        /// <param name="inValues"></param>
+        /// <returns></returns>
+        public double LargestDouble(double[] inValues)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Write a method that generates and returns an array of fifty integer values
+        /// </summary>
+        /// <returns></returns>
+        public int[] ReturnFiftyInts()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that takes two bool variables and returns true if they have the same value, false otherwis
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public bool BothSame(bool a, bool b)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that takes an int and a double and returns their product
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double ProductOfIntAndDouble(int a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that takes a two-dimensional array of integers and returns the average of the entries
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public int AverageOfMultiDimensionalArray(int[][] a)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
