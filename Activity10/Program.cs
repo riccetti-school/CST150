d0b887fb84d37199c75e8afb4e6407e15c43fc8a
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-10-03 10:01:25 -0500

    Activity10

---
+++ Activity10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnionInts(new[] { 1, 3, 5 }, new[] { 1, 2, 3, 4, 5, 6 }).ToList().ForEach(c => Console.WriteLine(c.ToString()));
+            Console.ReadKey();
+        }
+
+
+        static int[] UnionInts(int[] a, int[] b)
+        {
+            var t = a.Union(b);
+            return t.ToArray();
+        }
+    }
+}
