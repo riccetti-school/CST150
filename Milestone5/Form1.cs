c42b7835dc47d4f2092080d166106dad9ace247f
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-10-17 09:42:32 -0500

    Milestone 5

---
+++ Milestone5/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone4
+{
+    public partial class Form1 : Form
+    {
+
+        private InventoryManager _im = new InventoryManager();
+        private static Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new InventoryManagerUnitTests().RunTests();
+        }
+                
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        /// <summary>
+        /// add a random item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _im.Add(new ItemObj
+            {
+                Description = RandomString(10),
+                IsDamaged = false,
+                PackageQty = new Random((int)DateTime.Now.Ticks).Next(10),
+                QtyOnHand = new Random((int)DateTime.Now.Ticks).Next(10),
+                Id = Guid.NewGuid(),
+                ToShip = false
+            });
+
+            FillItems();
+        }
+
+        private void FillItems()
+        {
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+
+            foreach(var i in _im.GetInventoryList())
+            {
+                comboBox1.Items.Add(i);
+                comboBox2.Items.Add(i);
+            }
+
+            try
+            {
+                comboBox1.SelectedIndex = 0;
+                comboBox2.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+            }
+
+            label3.Text = $"Inventory Count: {_im.GetInventoryCount()}";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _im.Display(true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = string.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _im.Remove(((ItemObj)comboBox2.SelectedItem).Id);
+
+            FillItems();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // get search list
+                var qty = int.Parse(textBox1.Text);
+                //var dmg = checkBox1.Checked;
+
+                var l = _im.Search(qty);
+
+                _im.Display(true, l);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nothing to display");
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var i = int.Parse(textBox2.Text);
+                var id = ((ItemObj)comboBox1.SelectedItem).Id;
+                _im.ReStock(i, id);
+
+                FillItems();
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Milestone5/InventoryManagerUnitTests.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone4
+{
+    public class InventoryManagerUnitTests
+    {
+        public InventoryManagerUnitTests()
+        {
+            
+        }
+
+        public void RunTests()
+        {
+            CheckAdd();
+            CheckRemove();
+            CheckDisplay();
+            CheckReStock();
+            CheckDamageSearch();
+            CheckSearch();
+        }
+
+        public void CheckDamageSearch()
+        {
+            var t = new InventoryManager();
+            var id = Guid.NewGuid();
+            t.Add(new ItemObj
+            {
+                Description = $"New Test Item: {DateTime.Now.Ticks}",
+                IsDamaged = false,
+                PackageQty = 8,
+                QtyOnHand = 64,
+                ToShip = false,
+                Id = id
+            });
+
+            t.ReStock(100, id);
+
+            // check for our item
+            var y = t.Search(-1);
+
+            if (y.Any(c => c.Id.ToString() == id.ToString()))
+                Debug.WriteLine("FAILED!!! - Found something we should not have");
+            else
+                Debug.WriteLine("We didn't find any damanaged items.");
+        }
+
+        public void CheckSearch()
+        {
+            var t = new InventoryManager();
+            var id = Guid.NewGuid();
+            t.Add(new ItemObj
+            {
+                Description = $"New Test Item: {DateTime.Now.Ticks}",
+                IsDamaged = false,
+                PackageQty = 8,
+                QtyOnHand = 64,
+                ToShip = false,
+                Id = id
+            });
+
+            t.ReStock(100, id);
+
+            // check for our item
+            var y = t.Search(164);
+
+            if (y.Any(c => c.Id.ToString() == id.ToString()))
+                Debug.WriteLine("We found our item from search");
+            else
+                Debug.WriteLine("FAILED!!! - Did not find item with quantity 164");
+        }
+
+        public void CheckReStock()
+        {
+            var t = new InventoryManager();
+            var id = Guid.NewGuid();
+            t.Add(new ItemObj
+            {
+                Description = $"New Test Item: {DateTime.Now.Ticks}",
+                IsDamaged = false,
+                PackageQty = 8,
+                QtyOnHand = 64,
+                ToShip = false,
+                Id = id
+            });
+
+            t.ReStock(100, id);
+
+            if (t.GetInventoryList()[0].QtyOnHand == 164)
+                Debug.WriteLine("We restocked!");
+            else
+                Debug.WriteLine("FAILED!!! - Did not restock item");
+        }
+
+        public void CheckDisplay()
+        {
+            var t = new InventoryManager();
+            var id = Guid.NewGuid();
+            t.Add(new ItemObj
+            {
+                Description = $"New Test Item: {DateTime.Now.Ticks}",
+                IsDamaged = false,
+                PackageQty = 8,
+                QtyOnHand = 64,
+                ToShip = false,
+                Id = id
+            });
+
+            // we will check to see if this throws an exception
+            try
+            {
+                t.Display();
+                Debug.WriteLine("Display ran!");
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("FAILED!!! Display failed to run!");
+            }
+        }
+
+        public void CheckRemove()
+        {
+            var t = new InventoryManager();
+            var id = Guid.NewGuid();
+            t.Add(new ItemObj
+            {
+                Description = $"New Test Item: {DateTime.Now.Ticks}",
+                IsDamaged = false,
+                PackageQty = 8,
+                QtyOnHand = 64,
+                ToShip = false,
+                Id = id
+            });
+
+            t.Remove(id);
+
+            if (t.GetInventoryCount() == 0)
+                Debug.WriteLine("We removed our item");
+            else
+                Debug.WriteLine("FAILED!!! Item was not removed");
+        }
+
+        /// <summary>
+        /// check the add function
+        /// </summary>
+        public void CheckAdd()
+        {
+            var t = new InventoryManager();
+            t.Add(new ItemObj
+            {
+                Description = $"New Test Item: {DateTime.Now.Ticks}",
+                IsDamaged = false,
+                PackageQty = 8,
+                QtyOnHand = 64,
+                ToShip = false,
+                Id = Guid.NewGuid()
+            });
+
+            if(t.GetInventoryCount() == 1)
+            {
+                Debug.WriteLine("We have our item!");
+                return;
+            }
+
+            Debug.WriteLine("FAILED!!!!!! No item found");
+        }
+    }
+}
