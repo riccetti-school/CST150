ce3fefba10e2ab5e19d1dcc9d60f37521e40590b
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-10-17 07:15:40 -0500

    drop downs done

--- Activity15/Form1.Designer.cs
+++ Activity15/Form1.Designer.cs
@@ -55,6 +55,7 @@ namespace Activity15
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(177, 23);
             this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // comboBox3
             // 
@@ -74,11 +75,12 @@ namespace Activity15
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.Location = new System.Drawing.Point(23, 265);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(349, 94);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Get Luck Number!";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
@@ -136,6 +138,7 @@ namespace Activity15
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Dominic Riccetti - Luck Numbers";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Activity15/Form1.cs
+++ Activity15/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,88 @@ namespace Activity15
 {
     public partial class Form1 : Form
     {
+
+        private bool _monthDone = false;
+
+        enum Colors
+        {
+            Red,
+            Orange,
+            Green,
+            Blue,
+            Black
+        };
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillYear();
+
+            FillMonth();
+
+            FillColor();
+        }
+
+        private void FillColor()
+        {
+            comboBox4.Items.Clear();
+            foreach (string name in Enum.GetNames(typeof(Colors)))
+            {
+                comboBox4.Items.Add(name);
+            }
+            comboBox4.SelectedIndex = 0;
+        }
+
+        private void FillMonth()
+        {
+            var months = new[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            comboBox2.Items.Clear();
+            foreach (var month in months)
+            {
+                comboBox2.Items.Add(month);
+            }
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void FillYear()
+        {
+            var baseYear = 1990;
+            comboBox1.Items.Clear();
+
+            while (baseYear <= DateTime.Now.Year)
+            {
+                comboBox1.Items.Add(baseYear.ToString());
+
+                baseYear++;
+            }
+
+            _monthDone = true;
+
+            // select first one
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!_monthDone) return;
+
+            // add the days for this month
+            var month = ((ComboBox)sender).SelectedItem.ToString();
+            var monthNumber = DateTime.ParseExact(month, "MMMM", CultureInfo.CurrentCulture).Month;
+            var days = DateTime.DaysInMonth(int.Parse(comboBox1.SelectedItem.ToString()), monthNumber);
+            var dayCounter = 1;
+            comboBox3.Items.Clear();
+            while (dayCounter <= days)
+            {
+                comboBox3.Items.Add(dayCounter.ToString());
+                dayCounter++;
+            }
+            comboBox3.SelectedIndex = 0;
+
+        }
     }
 }
